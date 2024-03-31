ca53fbcb62b00f0023374088fd8330a19542864c
Karol <113446780+KarolOzga@users.noreply.github.com>
2024-03-31 11:15:52 +0000

    tut

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,20 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(number: i32)-> i32 {
+    let mut wynik: i32 = 0;
+    for i in 0..number{
+        if number <= 40{
+            wynik += 2;
+        }
+        else{
+            wynik += 1;
+        }
+    }
+    wynik
+}
 
 // Don't modify this function!
 #[test]
